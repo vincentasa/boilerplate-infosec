f34999432b506e97e2a35980dc3acae627ec0f55
vincentas a <vincentas.augustas@gmail.com>
2025-07-12 19:49:12 +0000

    oooo

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-app.use(helmet.hidePoweredBy());
+app.use(helmet.hidePoweredBy('X-Powered-By'));
 
 
 
