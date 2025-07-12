c9910fe53fc30fdb1b4a861105ec8bf17f9967aa
vincentas a <vincentas.augustas@gmail.com>
2025-07-12 19:58:05 +0000

    lmao

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const app = express();
 const helmet = require('helmet');
-
+const app = express();
 app.use(helmet.hidePoweredBy());
 
 
@@ -49,10 +48,8 @@ app.use(helmet.hidePoweredBy());
 
 
 
-module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
-app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
@@ -61,3 +58,4 @@ let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+module.exports = app;
